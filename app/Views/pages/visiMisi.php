cd5b48c246768365d88ebcbc124baea2cbf55365
hidayat307 <hidayatullahrazak07@gmail.com>
2024-11-06 21:52:58 +0800

    visimisi

--- app/Views/pages/visiMisi.php
+++ app/Views/pages/visiMisi.php
@@ -1,3 +1,17 @@
 <section class="pt-20 lg:pt-0">
     <h1>Visi dan Misi</h1>
+    <h4>
+        <strong> Visi </strong>
+    </h4>
+    <p>
+        <span style="font-weight: normal;">Melahirkan generasi cerdas, mandiri dan berakhlak Qurani</span>
+    </p>
+
+    <h4>
+        <strong> Misi </strong>
+    </h4>
+    <ol>
+        <li>Menyelenggarakan pendidikan islam yang memadukan aspek keimanan, keilmuan dan implementasinya </li>
+    </ol>
+
 </section>
