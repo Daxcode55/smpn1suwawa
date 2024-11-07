2f3294a8595555fe042dfc8dbe559b1b458fa17b
unknown <irgifebriansyah171@gmail.com>
2024-11-07 15:02:36 +0800

    sambutan kepala sekolah

--- app/Views/pages/sambutan.php
+++ app/Views/pages/sambutan.php
@@ -1,3 +1,19 @@
 <section class="pt-20 lg:pt-0">
     <h1>Sambutan kepala sekolah</h1>
+    <h4>Assalamualaikum warahmatullahi wabarakatuh.
+Salam sejahtera bagi kita semua.
+
+Puji dan syukur kita panjatkan kehadirat Allah SWT yang senantiasa melimpahkan rahmat dan karunia-Nya kepada kita semua. Dengan segala kerendahan hati, saya, selaku Kepala Sekolah SMPN 1 Suwawa, menyambut kehadiran Bapak/Ibu, serta seluruh siswa-siswi yang mengunjungi situs resmi SMPN 1 Suwawa.
+
+SMPN 1 Suwawa berkomitmen untuk menjadi lembaga pendidikan yang unggul dalam memberikan pembelajaran berkualitas serta menciptakan generasi muda yang berkarakter, berintegritas, dan siap menghadapi tantangan global. Kami percaya bahwa setiap anak memiliki potensi besar untuk berkembang, dan di sini, kami berusaha untuk membantu mereka mengoptimalkan potensi tersebut melalui proses pendidikan yang komprehensif dan didukung oleh berbagai fasilitas yang memadai.
+
+Sebagai salah satu sekolah menengah pertama di Kabupaten [Nama Kabupaten], kami bangga bisa memberikan kontribusi bagi masyarakat sekitar dengan mencetak lulusan yang berprestasi, baik dalam bidang akademik maupun non-akademik. Kami terus berupaya untuk meningkatkan kualitas pendidikan melalui berbagai program inovatif dan kegiatan ekstrakurikuler yang mendukung pengembangan karakter siswa.
+
+Saya berharap situs ini dapat menjadi media informasi dan komunikasi yang efektif antara sekolah dengan masyarakat luas, orang tua, serta para siswa. Melalui situs ini, kami akan selalu berupaya menyampaikan informasi terkini mengenai berbagai kegiatan, prestasi, dan program-program unggulan di SMPN 1 Suwawa. Semoga melalui kolaborasi antara guru, siswa, orang tua, dan masyarakat, kita dapat membawa SMPN 1 Suwawa menuju masa depan yang lebih gemilang.
+
+Akhir kata, saya ucapkan terima kasih atas dukungan dan kepercayaan yang telah diberikan kepada SMPN 1 Suwawa. Mari kita bersama-sama menciptakan lingkungan pendidikan yang kondusif, nyaman, dan penuh semangat dalam membimbing generasi muda bangsa.
+
+Wassalamualaikum warahmatullahi wabarakatuh.
+Salam hangat,
+Kepala Sekolah SMPN 1 Suwawa</h4>
 </section>
